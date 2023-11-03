033ec1b568eb69f31980811c42a8d3b6fc48d670
igl-myrick <igl.myrick@gmail.com>
2023-11-03 14:02:33 -0700

    refactor code into separate sections + fix display bug

--- js/scripts.js
+++ js/scripts.js
@@ -1,45 +1,48 @@
+// business logic
+function getDiscountedPrice(chosenProduct) {
+  let price;
+  let discountAmount;
+  if (chosenProduct === "sofa") {
+    price = 80;
+    discountAmount = 16;
+  } else if (chosenProduct === "chair") {
+    price = 50;
+    discountAmount = 10;
+  } else if (chosenProduct === "table") {
+    price = 100;
+    discountAmount = 20;
+  }
+  return price - discountAmount;
+}
+
+// ui logic
 window.addEventListener("load", function() {
   let productForm = document.querySelector("form#productForm");
   let purchaseForm = document.querySelector("form#purchaseInfo");
-  let userProduct = document.querySelector("input[name='product']:checked").value;
-  let discountedPrice;
   
-  let price;
-  if (userProduct === "sofa") {
-    price = 80;
-  } else if (userProduct === "chair") {
-    price = 50;
-  } else if (userProduct === "table") {
-    price = 100;
-  }
-  
-  function productSelection(discount) {
+  function productSelection(userProduct) {
     document.getElementById("userSelection").innerText = userProduct;
-    discountedPrice = (price - discount);
-    document.getElementById("discount").innerText = "DISCOUNT: We currently have a 20% discount running on " + userProduct + "s! Your order has been reduced to $" + discountedPrice + ".";
+    document.getElementById("discount").innerText = "DISCOUNT: We currently have a 20% discount running on " + userProduct + "s! Your order has been reduced to $" + getDiscountedPrice(userProduct) + ".";
   }
 
   productForm.addEventListener("submit", function(e) {
     e.preventDefault();
     document.querySelector("div#userOrder").removeAttribute("class");
+    document.querySelector("div#receipt").setAttribute("class", "hidden");
+    let chosenUserProduct = document.querySelector("input[name='product']:checked").value;
     
-    if (userProduct === "sofa") {
-      productSelection(16);
-    } else if (userProduct === "chair") {
-      productSelection(10);
-    } else if (userProduct === "table") {
-      productSelection(20);
-    }
+    productSelection(chosenUserProduct);
   });
 
   purchaseForm.addEventListener("submit", function(e) {
     e.preventDefault();
     document.querySelector("div#receipt").removeAttribute("class");
+    let userProduct = document.querySelector("input[name='product']:checked").value;
 
     const userShippingInfo = document.getElementById("shipping").value;
     const userPaymentInfo = document.getElementById("paymentInfo").value;
 
-    document.getElementById("userReceipt1").innerText = userProduct.toUpperCase() + " - $" + discountedPrice;
+    document.getElementById("userReceipt1").innerText = userProduct.toUpperCase() + " - $" + getDiscountedPrice(userProduct);
     document.getElementById("userReceipt2").innerText = userShippingInfo.toUpperCase();
     document.getElementById("userReceipt3").innerText = userPaymentInfo.toUpperCase();
   });
